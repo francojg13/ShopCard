d0dabd0e8a084a8d7ef3e56893229b32fb1b9f78
Franco <38586596+Zempro13@users.noreply.github.com>
2023-12-27 21:16:14 -0300

    add: package

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => { // Componente para el pie de página
           © {new Date().getFullYear()} Franco Galván
         </Typography>
         <Typography variant="body2" color="#1976d2" align="center">
-          <Link underline="hover" color="#1976d2" href="https://www.linkedin.com/in/francojg13/" target="_blank"> Seguime en LinkedIn
+          <Link underline="hover" color="#1976d2" href="https://www.linkedin.com/in/francojg13/" target="_blank"> LinkedIn
           </Link>{" "}
           |{" "}
           <Link underline="hover" color="#1976d2" href="#" target="_blank"> Política de Privacidad
